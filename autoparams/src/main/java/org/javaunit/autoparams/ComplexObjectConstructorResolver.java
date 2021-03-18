346a7ac9c68ea38ea15c449d25265adc2a29000a
Jin-Wook Chung <jwchung@hotmail.com>
2021-03-18 12:33:48 +0900

    Delete the isSimpleType method
    The isSimpleType check is unnecessary as generating simples types are
    considered before than complex types.

--- autoparams/src/main/java/org/javaunit/autoparams/ComplexObjectConstructorResolver.java
+++ autoparams/src/main/java/org/javaunit/autoparams/ComplexObjectConstructorResolver.java
@@ -4,13 +4,8 @@ import static java.util.Arrays.stream;
 
 import java.beans.ConstructorProperties;
 import java.lang.reflect.Constructor;
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
 
 final class ComplexObjectConstructorResolver {
 
@@ -21,17 +16,7 @@ final class ComplexObjectConstructorResolver {
     }
 
     public static Optional<Constructor<?>> resolveConstructor(Class<?> type) {
-        return isSimpleType(type)
-            ? Optional.empty()
-            : selectConstructor(type.getConstructors());
-    }
-
-    private static final Set<Class<?>> SIMPLE_TYPES = new HashSet<>(
-        Arrays.asList(Boolean.class, Integer.class, Long.class, Float.class, Double.class,
-            String.class, BigDecimal.class, UUID.class));
-
-    private static boolean isSimpleType(Class<?> type) {
-        return SIMPLE_TYPES.stream().anyMatch(type::equals);
+        return selectConstructor(type.getConstructors());
     }
 
     private static Optional<Constructor<?>> selectConstructor(Constructor<?>[] constructors) {
@@ -62,8 +47,8 @@ final class ComplexObjectConstructorResolver {
         Constructor<?>[] constructors) {
 
         return stream(constructors)
-           .filter(c -> c.isAnnotationPresent(ConstructorProperties.class))
-           .toArray(Constructor<?>[]::new);
+            .filter(c -> c.isAnnotationPresent(ConstructorProperties.class))
+            .toArray(Constructor<?>[]::new);
     }
 
 }
