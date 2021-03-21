30b4e47f5a16d7418bab0beb787b3cb3ad617a3b
mhyeon-lee <mhyeon.lee@navercorp.com>
2021-03-21 22:38:21 +0900

    [#84] Add Builder

---
+++ autoparams/src/main/java/org/javaunit/autoparams/Builder.java
@@ -0,0 +1,36 @@
+package org.javaunit.autoparams;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+public final class Builder<T> {
+
+    private final GenericObjectQuery builderQuery;
+    private final ObjectGenerationContext context;
+
+    Builder(GenericObjectQuery builderQuery, ObjectGenerationContext context) {
+        this.builderQuery = builderQuery;
+        this.context = context;
+    }
+
+    public T build() {
+        return generate(this.builderQuery);
+    }
+
+    @SuppressWarnings("unchecked")
+    private T generate(GenericObjectQuery builderQuery) {
+        ObjectQuery query = this.createGenerateObjectQuery(builderQuery);
+        return (T) this.context.generate(query);
+    }
+
+    private ObjectQuery createGenerateObjectQuery(GenericObjectQuery builderQuery) {
+        Type generateType = builderQuery.getParameterizedType().getActualTypeArguments()[0];
+        if (generateType instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) generateType;
+            return new GenericObjectQuery(
+                (Class<?>) parameterizedType.getRawType(), parameterizedType);
+        } else {
+            return new ObjectQuery((Class<?>) generateType);
+        }
+    }
+}
