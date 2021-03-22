a4b5fa521df86e98c20fba326f9d8c4f981a5c70
arron-shin <arron.shin@gmail.com>
2021-03-22 19:30:38 +0900

    [#71] short 값 생성 지원

---
+++ autoparams/src/main/java/org/javaunit/autoparams/ShortGenerator.java
@@ -0,0 +1,18 @@
+package org.javaunit.autoparams;
+
+import java.util.Optional;
+
+final class ShortGenerator implements ObjectGenerator {
+
+    public static short factory() {
+        return (short) RANDOM.nextInt(Short.MIN_VALUE, Short.MAX_VALUE + 1);
+    }
+
+    @Override
+    public Optional<Object> generate(ObjectQuery query, ObjectGenerationContext context) {
+        Class<?> type = query.getType();
+        return type.equals(short.class) || type.equals(Short.class)
+               ? Optional.of(factory())
+               : Optional.empty();
+    }
+}
