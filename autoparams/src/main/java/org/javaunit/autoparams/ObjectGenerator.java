9f7f3c3c8c3965ff9853205d005f75c9a32af172
Jin-Wook Chung <jwchung@hotmail.com>
2021-03-24 13:43:54 +0900

    Delete the unnecessary comment

--- autoparams/src/main/java/org/javaunit/autoparams/ObjectGenerator.java
+++ autoparams/src/main/java/org/javaunit/autoparams/ObjectGenerator.java
@@ -7,13 +7,6 @@ interface ObjectGenerator {
 
     ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
 
-    /**
-     * This method generates an arbitrary object with the given query.
-     *
-     * @deprecated This method will be deleted, so don't implement it, but implement
-     *     the {@link ObjectGenerator#generateObject(ObjectQuery, ObjectGenerationContext)} method
-     *     instead.
-     */
     Optional<Object> generate(ObjectQuery query, ObjectGenerationContext context);
 
     default GenerationResult generateObject(ObjectQuery query, ObjectGenerationContext context) {
