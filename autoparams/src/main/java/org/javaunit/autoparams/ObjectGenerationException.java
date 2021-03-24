8681a65dc9564f9e705e7c0b9bc7a82057fa1702
Gyuwon Yi <gyuwon@live.com>
2021-03-24 16:07:55 +0900

    Fix warning in ObjectGenerationException class
    The detail message of the warning:
    
    The serializable class ObjectGenerationException does not declare a
    static final serialVersionUID field of type longJava(536871008)

--- autoparams/src/main/java/org/javaunit/autoparams/ObjectGenerationException.java
+++ autoparams/src/main/java/org/javaunit/autoparams/ObjectGenerationException.java
@@ -1,7 +1,11 @@
 package org.javaunit.autoparams;
 
 public final class ObjectGenerationException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
     ObjectGenerationException(String message) {
         super(message);
     }
+
 }
