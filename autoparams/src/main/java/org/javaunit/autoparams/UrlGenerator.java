53feb503e775f285ecf1ace7d55ce7b03fb84d5b
Jin-Wook Chung <jwchung@hotmail.com>
2021-03-25 22:07:07 +0900

    Implement the UrlGenerator class
    This is the first task to only support various protocols. In the
    following tasks, various domains and pathes will be supported.

---
+++ autoparams/src/main/java/org/javaunit/autoparams/UrlGenerator.java
@@ -0,0 +1,34 @@
+package org.javaunit.autoparams;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Optional;
+
+final class UrlGenerator implements ObjectGenerator {
+
+    private static final String[] PROTOCOLS = new String[] {
+        "http",
+        "https",
+        "ftp"
+    };
+
+    @Override
+    public Optional<Object> generate(ObjectQuery query, ObjectGenerationContext context) {
+        return query.getType().equals(URL.class)
+            ? Optional.of(generate())
+            : Optional.empty();
+    }
+
+    private URL generate() {
+        int index = RANDOM.nextInt(PROTOCOLS.length);
+        String urlSource = String.format("%s://auto.params", PROTOCOLS[index]);
+
+        try {
+            return new URL(urlSource);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
