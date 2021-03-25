f12d66973f1cf899ac239ddf0366406a048a29c0
Jin-Wook Chung <jwchung@hotmail.com>
2021-03-25 23:20:16 +0900

    Use the Builder class

--- autoparams/src/test/java/org/javaunit/autoparams/UrlGenerationSpecs.java
+++ autoparams/src/test/java/org/javaunit/autoparams/UrlGenerationSpecs.java
@@ -3,25 +3,16 @@ package org.javaunit.autoparams;
 import static org.assertj.core.api.BDDAssertions.then;
 
 import java.net.URL;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 import org.junit.jupiter.params.ParameterizedTest;
 
 public class UrlGenerationSpecs {
 
     @ParameterizedTest
     @AutoSource
-    void sut_supports_various_protocols(
-        List<URL> x,
-        List<URL> y,
-        List<URL> z,
-        List<URL> l,
-        List<URL> m,
-        List<URL> n
-    ) {
-        long actual = Stream.of(x, y, z, l, m, n)
-            .flatMap(Collection::stream)
+    void sut_supports_various_protocols(Builder<URL> builder) {
+        long actual = IntStream.range(0, 100)
+            .mapToObj(it -> builder.build())
             .map(URL::getProtocol)
             .distinct()
             .count();
