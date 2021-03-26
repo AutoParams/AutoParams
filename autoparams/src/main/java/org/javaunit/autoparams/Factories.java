5ce6591d28d797a0ab88192df3c5d7f960fb7720
Gyuwon Yi <gyuwon@live.com>
2021-03-26 23:44:14 +0900

    Refactor primitive value generator

--- autoparams/src/main/java/org/javaunit/autoparams/Factories.java
+++ autoparams/src/main/java/org/javaunit/autoparams/Factories.java
@@ -14,4 +14,28 @@ final class Factories {
         return (byte) RANDOM.nextInt(Byte.MIN_VALUE, Byte.MAX_VALUE + 1);
     }
 
+    public static short createShort() {
+        return (short) RANDOM.nextInt(Short.MIN_VALUE, Short.MAX_VALUE + 1);
+    }
+
+    public static int createInt() {
+        return RANDOM.nextInt();
+    }
+
+    public static long createLong() {
+        return RANDOM.nextLong();
+    }
+
+    public static float createFloat() {
+        return RANDOM.nextFloat();
+    }
+
+    public static double createDouble() {
+        return RANDOM.nextDouble();
+    }
+
+    public static char createChar() {
+        return (char) RANDOM.nextInt(Character.MAX_VALUE + 1);
+    }
+
 }
