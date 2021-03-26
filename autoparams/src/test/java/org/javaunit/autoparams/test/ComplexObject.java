40ee8cd7084c6b4341bfb4947506e2d979836f36
Gyuwon Yi <gyuwon@live.com>
2021-03-26 22:25:57 +0900

    Rewrite tests for complex objects

---
+++ autoparams/src/test/java/org/javaunit/autoparams/test/ComplexObject.java
@@ -0,0 +1,29 @@
+package org.javaunit.autoparams.test;
+
+import java.util.UUID;
+
+public class ComplexObject {
+
+    private final int value1;
+    private final String value2;
+    private final UUID value3;
+
+    public ComplexObject(int value1, String value2, UUID value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public int getValue1() {
+        return value1;
+    }
+
+    public String getValue2() {
+        return value2;
+    }
+
+    public UUID getValue3() {
+        return value3;
+    }
+
+}
