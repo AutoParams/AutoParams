8bdda2cb629efb79da63bf7e8781f3440a8fa3da
Gyuwon Yi <gyuwon@live.com>
2021-03-27 00:05:56 +0900

    Refactor EnumGenerator

--- autoparams/src/main/java/org/javaunit/autoparams/EnumGenerator.java
+++ autoparams/src/main/java/org/javaunit/autoparams/EnumGenerator.java
@@ -1,20 +1,30 @@
 package org.javaunit.autoparams;
 
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 final class EnumGenerator implements ObjectGenerator {
 
     @Override
     public Optional<Object> generate(ObjectQuery query, ObjectGenerationContext context) {
-        Class<?> superType = query.getType().getSuperclass();
-        return superType != null && superType.equals(Enum.class)
-            ? factory(query.getType())
-            : Optional.empty();
+        String message = "This method is not supported. Use generateObject method instead.";
+        throw new UnsupportedOperationException(message);
     }
 
-    private Optional<Object> factory(Class<?> type) {
+    @Override
+    public final GenerationResult generateObject(
+        ObjectQuery query,
+        ObjectGenerationContext context
+    ) {
+        Class<?> superType = query.getType().getSuperclass();
+        return superType != null && superType.equals(Enum.class)
+            ? GenerationResult.presence(factory(query.getType()))
+            : GenerationResult.absence();
+    }
+
+    private Object factory(Class<?> type) {
         Object[] values = EnumValuesResolver.resolveValues(type);
-        int index = RANDOM.nextInt(values.length);
-        return Optional.of(values[index]);
+        int index = ThreadLocalRandom.current().nextInt(values.length);
+        return values[index];
     }
 }
