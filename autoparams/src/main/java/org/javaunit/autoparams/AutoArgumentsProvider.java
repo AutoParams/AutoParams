fb9bfc67057b13b9c95fa36951f15b5871e72ea3
Gyuwon Yi <gyuwon@live.com>
2021-03-26 23:44:14 +0900

    Refactor primitive value generator

--- autoparams/src/main/java/org/javaunit/autoparams/AutoArgumentsProvider.java
+++ autoparams/src/main/java/org/javaunit/autoparams/AutoArgumentsProvider.java
@@ -19,12 +19,12 @@ final class AutoArgumentsProvider implements ArgumentsProvider,
         new CompositeObjectGenerator(
             new TypeMatchingGenerator(Factories::createBoolean, boolean.class, Boolean.class),
             new TypeMatchingGenerator(Factories::createByte, byte.class, Byte.class),
-            new ShortGenerator(),
-            new IntegerGenerator(),
-            new LongGenerator(),
-            new FloatGenerator(),
-            new DoubleGenerator(),
-            new CharacterGenerator());
+            new TypeMatchingGenerator(Factories::createShort, short.class, Short.class),
+            new TypeMatchingGenerator(Factories::createInt, int.class, Integer.class),
+            new TypeMatchingGenerator(Factories::createLong, long.class, Long.class),
+            new TypeMatchingGenerator(Factories::createFloat, float.class, Float.class),
+            new TypeMatchingGenerator(Factories::createDouble, double.class, Double.class),
+            new TypeMatchingGenerator(Factories::createChar, char.class, Character.class));
 
     private static final ObjectGenerator SIMPLE_VALUE_OBJECT_GENERATOR =
         new CompositeObjectGenerator(
