0d6f44696ee2e40c8938578ff3dea3809b28f587
Gyuwon Yi <gyuwon@live.com>
2021-03-27 20:05:28 +0900

    Rewrite tests for arrays

--- autoparams/src/test/java/org/javaunit/autoparams/AutoSourceSpecs.java
+++ autoparams/src/test/java/org/javaunit/autoparams/AutoSourceSpecs.java
@@ -22,57 +22,6 @@ public class AutoSourceSpecs {
 
     @ParameterizedTest
     @AutoSource
-    void sut_correctly_creates_array_of_int(int[] array) {
-        assertThat(array).hasSize(3);
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_fills_array_of_int_with_arbitrary_values(int[] array) {
-        HashSet<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < array.length; i++) {
-            set.add(array[i]);
-        }
-
-        assertThat(set).hasSize(array.length);
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_fills_array_of_Integer_with_arbitrary_values(Integer[] array) {
-        HashSet<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < array.length; i++) {
-            set.add(array[i]);
-        }
-
-        assertThat(set).hasSize(array.length);
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_fills_array_of_complex_type_with_arbitrary_objects(ComplexObject[] array) {
-        HashSet<ComplexObject> set = new HashSet<ComplexObject>();
-        for (int i = 0; i < array.length; i++) {
-            set.add(array[i]);
-        }
-
-        assertThat(set).hasSize(array.length);
-    }
-
-    @ParameterizedTest
-    @AutoSource(repeat = 10)
-    void sut_fills_array_of_enum_type_with_arbitrary_objects(EnumType[] array) {
-        HashSet<EnumType> set = new HashSet<>();
-        for (int i = 0; i < array.length; i++) {
-            set.add(array[i]);
-        }
-
-        assertThat(set.size()).isGreaterThanOrEqualTo(1);
-        assertThat(set.size()).isLessThanOrEqualTo(EnumType.values().length);
-    }
-
-    @ParameterizedTest
-    @AutoSource
     void sut_creates_array_list(ArrayList<ComplexObject> arrayList) {
         assertThat(arrayList).isNotNull();
     }
