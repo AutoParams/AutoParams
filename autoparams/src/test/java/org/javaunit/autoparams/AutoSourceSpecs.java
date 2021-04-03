f9c560721445d9d2d3f736569f36a8623f8324ce
Gyuwon Yi <gyuwon@live.com>
2021-04-03 13:26:57 +0900

    Move test for failures to the test package

--- autoparams/src/test/java/org/javaunit/autoparams/AutoSourceSpecs.java
+++ autoparams/src/test/java/org/javaunit/autoparams/AutoSourceSpecs.java
@@ -1,9 +1,7 @@
 package org.javaunit.autoparams;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.AbstractList;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -38,36 +36,6 @@ public class AutoSourceSpecs {
     ) {
         Map<String, ComplexObject> value = builder.build();
         assertThat(value).isNotEmpty();
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_throws_when_object_of_interface_is_requested(
-        Builder<Cloneable> builder
-    ) {
-        assertThatThrownBy(builder::build)
-            .isInstanceOf(ObjectGenerationException.class)
-            .hasMessageContaining("interface");
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_throws_when_object_of_abstract_class_is_requested(
-        Builder<AbstractList<Object>> builder
-    ) {
-        assertThatThrownBy(builder::build)
-            .isInstanceOf(ObjectGenerationException.class)
-            .hasMessageContaining("abstract");
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_throws_when_object_of_abstract_class_with_public_constructor_is_requested(
-        Builder<AbstractWithPublicConstructor> builder
-    ) {
-        assertThatThrownBy(builder::build)
-            .isInstanceOf(ObjectGenerationException.class)
-            .hasMessageContaining("abstract");
     }
 
 }
