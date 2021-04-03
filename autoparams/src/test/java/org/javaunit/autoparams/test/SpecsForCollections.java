b85e26e11a7229798c503c07e2e0781aff87f245
Gyuwon Yi <gyuwon@live.com>
2021-04-03 13:04:28 +0900

    Move tests for collections to the test package

---
+++ autoparams/src/test/java/org/javaunit/autoparams/test/SpecsForCollections.java
@@ -0,0 +1,92 @@
+package org.javaunit.autoparams.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import org.javaunit.autoparams.AutoSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class SpecsForCollections {
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_array_list(ArrayList<ComplexObject> arrayList) {
+        assertThat(arrayList).isNotNull();
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_fills_array_list(ArrayList<UUID> arrayList) {
+        HashSet<UUID> set = new HashSet<UUID>();
+        for (UUID x : arrayList) {
+            set.add(x);
+        }
+
+        assertThat(arrayList).hasSize(3);
+        assertThat(set).hasSize(arrayList.size());
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_list(List<ComplexObject> list) {
+        assertThat(list).isNotNull();
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_collection(Collection<ComplexObject> collection) {
+        assertThat(collection).isNotNull();
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_iterable(Iterable<ComplexObject> iterable) {
+        assertThat(iterable).isNotNull();
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_hash_map(HashMap<Integer, String> map) {
+        assertThat(map).isNotNull();
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_correctly_fills_hash_map(HashMap<Integer, String> map) {
+        assertThat(map).hasSize(3);
+        HashSet<String> set = new HashSet<String>(map.values());
+        assertThat(set).hasSize(map.keySet().size());
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_map(Map<Integer, String> map) {
+        assertThat(map).isNotNull();
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_hash_set(HashSet<String> set) {
+        assertThat(set).isNotNull();
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_correctly_fills_hash_set(HashSet<String> set) {
+        assertThat(set).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_set(Set<String> set) {
+        assertThat(set).isNotNull();
+    }
+
+}
