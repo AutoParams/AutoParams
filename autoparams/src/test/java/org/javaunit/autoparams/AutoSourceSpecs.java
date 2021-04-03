d149588dd5897f795d0d1bf06f1245f8ec230bc8
Gyuwon Yi <gyuwon@live.com>
2021-04-03 13:20:17 +0900

    Move tests for streams to the test package

--- autoparams/src/test/java/org/javaunit/autoparams/AutoSourceSpecs.java
+++ autoparams/src/test/java/org/javaunit/autoparams/AutoSourceSpecs.java
@@ -4,53 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.AbstractList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 
 public class AutoSourceSpecs {
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_creates_intStream(IntStream intStream) {
-        assertThat(intStream).isNotNull();
-        assertThat(intStream).hasSize(3);
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_creates_longStream(LongStream longStream) {
-        assertThat(longStream).isNotNull();
-        assertThat(longStream).hasSize(3);
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_creates_doubleStream(DoubleStream doubleStream) {
-        assertThat(doubleStream).isNotNull();
-        assertThat(doubleStream).hasSize(3);
-    }
-
-    @ParameterizedTest
-    @AutoSource
-    void sut_creates_stream(Stream<UUID> stream) {
-        assertThat(stream).isNotNull();
-
-        Object[] array = stream.toArray();
-        HashSet<UUID> set = new HashSet<UUID>();
-        for (Object element : array) {
-            set.add((UUID) element);
-        }
-
-        assertThat(array).hasSize(3);
-        assertThat(set).hasSize(array.length);
-    }
 
     @ParameterizedTest
     @AutoSource
---
+++ autoparams/src/test/java/org/javaunit/autoparams/test/SpecsForStreams.java
@@ -0,0 +1,52 @@
+package org.javaunit.autoparams.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.UUID;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+import org.javaunit.autoparams.AutoSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class SpecsForStreams {
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_intStream(IntStream intStream) {
+        assertThat(intStream).isNotNull();
+        assertThat(intStream).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_longStream(LongStream longStream) {
+        assertThat(longStream).isNotNull();
+        assertThat(longStream).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_doubleStream(DoubleStream doubleStream) {
+        assertThat(doubleStream).isNotNull();
+        assertThat(doubleStream).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_creates_generic_stream(Stream<UUID> stream) {
+        assertThat(stream).isNotNull();
+
+        Object[] array = stream.toArray();
+        HashSet<UUID> set = new HashSet<UUID>();
+        for (Object element : array) {
+            set.add((UUID) element);
+        }
+
+        assertThat(array).hasSize(3);
+        assertThat(set).hasSize(array.length);
+    }
+
+}
