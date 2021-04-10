669b8e241b0d2399aa6272f16fa9e1b58244e511
Jin-Wook Chung <jwchung@hotmail.com>
2021-04-10 14:10:47 +0900

    Support port numbers for URLs

--- autoparams/src/test/java/org/javaunit/autoparams/test/SpecsForUrl.java
+++ autoparams/src/test/java/org/javaunit/autoparams/test/SpecsForUrl.java
@@ -1,6 +1,7 @@
 package org.javaunit.autoparams.test;
 
-import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.offset;
 
 import java.net.URL;
 import java.util.stream.IntStream;
@@ -19,7 +20,30 @@ class SpecsForUrl {
             .distinct()
             .count();
 
-        then(actual).isGreaterThanOrEqualTo(3);
+        assertThat(actual).isGreaterThanOrEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_supports_various_ports(Builder<URL> builder) {
+        long actual = IntStream.range(0, 100)
+            .mapToObj(it -> builder.build())
+            .map(URL::getPort)
+            .distinct()
+            .count();
+
+        assertThat(actual).isGreaterThanOrEqualTo(10);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_generates_urls_with_port_numbers_about_50_percent(Builder<URL> builder) {
+        long actual = IntStream.range(0, 100)
+            .mapToObj(it -> builder.build())
+            .filter(it -> it.getPort() >= 0)
+            .count();
+
+        assertThat(actual / (double) 100).isEqualTo(0.5, offset(0.2));
     }
 
 }
