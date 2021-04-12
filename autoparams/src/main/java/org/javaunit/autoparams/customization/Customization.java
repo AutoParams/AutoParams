798c6b44d0bfbe8f27bd94432e9ea04791f90077
Gyuwon Yi <gyuwon@live.com>
2021-04-12 22:10:34 +0900

    Introduce @Customization annotation
    @Customization annotation declares Customizer types to apply to
    @AutoSource.

---
+++ autoparams/src/main/java/org/javaunit/autoparams/customization/Customization.java
@@ -0,0 +1,14 @@
+package org.javaunit.autoparams.customization;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Customization {
+
+    Class<? extends Customizer>[] value();
+
+}
