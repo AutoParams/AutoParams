ea435ca2466d03bbda9afde98a84c7824e228b37
Gyuwon Yi <gyuwon@live.com>
2021-04-15 21:48:11 +0900

    @AutoSource accepts @Customization

--- autoparams/src/main/java/org/javaunit/autoparams/AutoArgumentsProvider.java
+++ autoparams/src/main/java/org/javaunit/autoparams/AutoArgumentsProvider.java
@@ -2,10 +2,13 @@ package org.javaunit.autoparams;
 
 import static java.util.Arrays.stream;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.Map;
 import java.util.stream.Stream;
+import org.javaunit.autoparams.customization.Customization;
+import org.javaunit.autoparams.customization.Customizer;
 import org.javaunit.autoparams.generator.UnwrapFailedException;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,45 +17,57 @@ import org.junit.jupiter.params.support.AnnotationConsumer;
 
 final class AutoArgumentsProvider implements ArgumentsProvider, AnnotationConsumer<AutoSource> {
 
-    private static final Stream<Arguments> EMPTY = stream(new Arguments[0]);
-
     private static class GeneratorAdapter implements ObjectGenerator {
+        public org.javaunit.autoparams.generator.ObjectGenerator generator;
+
+        public GeneratorAdapter() {
+            generator = org.javaunit.autoparams.generator.ObjectGenerator.DEFAULT;
+        }
 
         @Override
         public GenerationResult generate(ObjectQuery query, ObjectGenerationContext context) {
             return GenerationResult.fromContainer(
-                org.javaunit.autoparams.generator.ObjectGenerator.DEFAULT
+                generator
                     .generate(
                         () -> query.getType(),
                         new org.javaunit.autoparams.generator.ObjectGenerationContext()));
         }
     }
 
-    public static final CompositeObjectGenerator DEFAULT_OBJECT_GENERATOR =
+    private final GeneratorAdapter adapter = new GeneratorAdapter();
+    private final CompositeObjectGenerator generator =
         new CompositeObjectGenerator(
-            new GeneratorAdapter(),
+            adapter,
             new SimpleValueObjectGenerator(),
             new CollectionGenerator(),
             new StreamGenerator(),
             new BuilderGenerator(),
             new ComplexObjectGenerator());
-
-    private final ObjectGenerationContext context;
-    private int repeat;
-
-    public AutoArgumentsProvider() {
-        this(new ObjectGenerationContext(DEFAULT_OBJECT_GENERATOR));
-    }
-
-    private AutoArgumentsProvider(ObjectGenerationContext context) {
-        this.context = context;
-        this.repeat = 1;
-    }
+    private final ObjectGenerationContext context = new ObjectGenerationContext(generator);
+    private int repeat = 1;
 
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+        Method method = context.getRequiredTestMethod();
+        customizeGenerator(method);
         applyFixedValues(context);
-        return context.getTestMethod().map(this::generate).orElse(EMPTY);
+        return generate(method);
+    }
+
+    private void customizeGenerator(Method method) {
+        Customization customization = method.getAnnotation(Customization.class);
+        if (customization != null) {
+            Class<? extends Customizer>[] types = customization.value();
+            for (Class<? extends Customizer> type : types) {
+                try {
+                    Customizer customizer = type.getDeclaredConstructor().newInstance();
+                    adapter.generator = customizer.customize(adapter.generator);
+                } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                        | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                    return;
+                }
+            }
+        }
     }
 
     private void applyFixedValues(ExtensionContext context) {
@@ -78,7 +93,7 @@ final class AutoArgumentsProvider implements ArgumentsProvider, AnnotationConsum
 
     private Object createArgument(Parameter parameter) {
         try {
-            return org.javaunit.autoparams.generator.ObjectGenerator.DEFAULT
+            return adapter.generator
                 .generate(
                     org.javaunit.autoparams.generator.ObjectQuery.fromParameter(parameter),
                     new org.javaunit.autoparams.generator.ObjectGenerationContext())
