bd89725b04ef6beb7a7773d198049cd035a3434f
Gyuwon Yi <gyuwon@live.com>
2021-04-17 21:36:51 +0900

    Add generator property to ObjectGenerationContext

--- autoparams/src/main/java/org/javaunit/autoparams/generator/ObjectGenerationContext.java
+++ autoparams/src/main/java/org/javaunit/autoparams/generator/ObjectGenerationContext.java
@@ -1,4 +1,15 @@
 package org.javaunit.autoparams.generator;
 
 public final class ObjectGenerationContext {
+
+    private final ObjectGenerator generator;
+
+    public ObjectGenerationContext(ObjectGenerator generator) {
+        this.generator = generator;
+    }
+
+    ObjectGenerator getGenerator() {
+        return generator;
+    }
+
 }
