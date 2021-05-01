57471d15fe4380150088ed88985b829d5285081b
Gyuwon Yi <gyuwon@live.com>
2021-05-01 18:02:25 +0900

    Extract foldl method

--- autoparams/src/main/java/org/javaunit/autoparams/generator/CompositeObjectGenerator.java
+++ autoparams/src/main/java/org/javaunit/autoparams/generator/CompositeObjectGenerator.java
@@ -1,9 +1,6 @@
 package org.javaunit.autoparams.generator;
 
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.function.BiFunction;
-import java.util.stream.Stream;
 
 public class CompositeObjectGenerator implements ObjectGenerator {
 
@@ -15,20 +12,11 @@ public class CompositeObjectGenerator implements ObjectGenerator {
 
     @Override
     public ObjectContainer generate(ObjectQuery query, ObjectGenerationContext context) {
-        return foldl(
+        return Folder.foldl(
             (result, generator) -> result.yieldIfEmpty(() -> generator.generate(query, context)),
             ObjectContainer.EMPTY,
             Arrays.stream(generators)
         );
-    }
-
-    private static <T, U> U foldl(BiFunction<U, T, U> f, U z, Stream<T> xs) {
-        Iterator<T> i = xs.iterator();
-        U a = z;
-        while (i.hasNext()) {
-            a = f.apply(a, i.next());
-        }
-        return a;
     }
 
 }
