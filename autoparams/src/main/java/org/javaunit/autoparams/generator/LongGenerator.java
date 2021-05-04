960c5272be5c2af0a4da2051031f0d1ce506d2d2
sogoagain <imyong0@gmail.com>
2021-05-04 22:33:08 +0900

    Support @Min constraint for parameter of long

---
+++ autoparams/src/main/java/org/javaunit/autoparams/generator/LongGenerator.java
@@ -0,0 +1,33 @@
+package org.javaunit.autoparams.generator;
+
+import java.lang.reflect.Type;
+import java.util.concurrent.ThreadLocalRandom;
+import javax.validation.constraints.Min;
+
+final class LongGenerator implements ObjectGenerator {
+
+    @Override
+    public ObjectContainer generate(ObjectQuery query, ObjectGenerationContext context) {
+        Type type = query.getType();
+        if (type == long.class || type == Long.class) {
+            long origin = getOrigin(query);
+            long value = ThreadLocalRandom.current().nextLong(origin, Long.MAX_VALUE);
+            return new ObjectContainer(value);
+        }
+
+        return ObjectContainer.EMPTY;
+    }
+
+    private long getOrigin(ObjectQuery query) {
+        if (query instanceof ArgumentQuery) {
+            ArgumentQuery argumentQuery = (ArgumentQuery) query;
+            Min annotation = argumentQuery.getParameter().getAnnotation(Min.class);
+            if (annotation != null) {
+                return Math.max(Long.MIN_VALUE, annotation.value());
+            }
+        }
+
+        return Long.MIN_VALUE;
+    }
+
+}
