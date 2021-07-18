718745354a4712b55173f7d3a02ec7e3fadb29f5
Gyuwon Yi <gyuwon@live.com>
2021-07-18 11:03:49 +0900

    MockitoCustomizer ignores collection types

--- autoparams-mockito/src/main/java/org/javaunit/autoparams/mockito/MockitoCustomizer.java
+++ autoparams-mockito/src/main/java/org/javaunit/autoparams/mockito/MockitoCustomizer.java
@@ -5,6 +5,14 @@ import static org.mockito.Mockito.mock;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.AbstractCollection;
+import java.util.AbstractList;
+import java.util.AbstractMap;
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.javaunit.autoparams.customization.Customizer;
 import org.javaunit.autoparams.generator.ObjectContainer;
 import org.javaunit.autoparams.generator.ObjectGenerator;
@@ -24,12 +32,26 @@ public final class MockitoCustomizer implements Customizer {
     }
 
     private ObjectContainer generate(Class<?> type) {
-        return isAbstract(type) ? new ObjectContainer(mock(type)) : ObjectContainer.EMPTY;
+        return isCollection(type) == false && isAbstract(type)
+            ? new ObjectContainer(mock(type))
+            : ObjectContainer.EMPTY;
     }
 
     private ObjectContainer generate(ParameterizedType parameterizedType) {
         Type type = parameterizedType.getRawType();
         return type instanceof Class<?> ? generate((Class<?>) type) : ObjectContainer.EMPTY;
+    }
+
+    private boolean isCollection(Class<?> type) {
+        return type.equals(Iterable.class)
+            || type.equals(Collection.class)
+            || type.equals(AbstractCollection.class)
+            || type.equals(List.class)
+            || type.equals(AbstractList.class)
+            || type.equals(Set.class)
+            || type.equals(AbstractSet.class)
+            || type.equals(Map.class)
+            || type.equals(AbstractMap.class);
     }
 
     private boolean isAbstract(Class<?> type) {
--- autoparams-mockito/src/test/java/org/javaunit/autoparams/mockito/test/SpecsForMockitoCustomizer.java
+++ autoparams-mockito/src/test/java/org/javaunit/autoparams/mockito/test/SpecsForMockitoCustomizer.java
@@ -1,8 +1,20 @@
 package org.javaunit.autoparams.mockito.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.AbstractCollection;
+import java.util.AbstractList;
+import java.util.AbstractMap;
+import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.IntSupplier;
 import org.javaunit.autoparams.AutoSource;
 import org.javaunit.autoparams.customization.Customization;
@@ -11,44 +23,107 @@ import org.junit.jupiter.params.ParameterizedTest;
 
 class SpecsForMockitoCustomizer {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
     @AutoSource
     @Customization(MockitoCustomizer.class)
     void sut_correctly_creates_value_for_argument_of_interface(IntSupplier value) {
         assertNotNull(value);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
     @AutoSource
     @Customization(MockitoCustomizer.class)
     void sut_yields_for_concrete_type(ConcreteClass value) {
         assertEquals(ConcreteClass.class, value.getClass());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
     @AutoSource
     @Customization(MockitoCustomizer.class)
     void sut_correctly_creates_value_for_argument_of_abstract_class(AbstractClass value) {
         assertNotNull(value);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
     @AutoSource
     @Customization(MockitoCustomizer.class)
     void sut_supports_generic_interface(GenericInterface<Integer> value) {
         assertNotNull(value);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
     @AutoSource
     @Customization(MockitoCustomizer.class)
     void sut_ignores_parameters_of_primitive_types(int arg1, double arg2) {
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
     @AutoSource
     @Customization(MockitoCustomizer.class)
     void sut_ignores_array(int[] arg1) {
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_iterable(Iterable<Integer> arg) {
+        assertThat(arg).isInstanceOf(ArrayList.class);
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_collection(Collection<Integer> arg) {
+        assertThat(arg).isInstanceOf(ArrayList.class);
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_abstract_collection(AbstractCollection<Integer> arg) {
+        assertThat(arg).isInstanceOf(ArrayList.class);
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_list(List<Integer> arg) {
+        assertThat(arg).isInstanceOf(ArrayList.class);
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_abstract_list(AbstractList<Integer> arg) {
+        assertThat(arg).isInstanceOf(ArrayList.class);
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_set(Set<Integer> arg) {
+        assertThat(arg).isInstanceOf(HashSet.class);
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_abstract_set(AbstractSet<Integer> arg) {
+        assertThat(arg).isInstanceOf(HashSet.class);
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_map(Map<String, Integer> arg) {
+        assertThat(arg).isInstanceOf(HashMap.class);
+    }
+
+    @ParameterizedTest(name = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER)
+    @AutoSource
+    @Customization(MockitoCustomizer.class)
+    void sut_ignores_abstract_map(AbstractMap<String, Integer> arg) {
+        assertThat(arg).isInstanceOf(HashMap.class);
     }
 
 }
