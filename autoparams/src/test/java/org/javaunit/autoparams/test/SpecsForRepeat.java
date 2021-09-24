8edfd5e6d232ecca5a12d87db6f992dc579087a6
Gyuwon Yi <gyuwon@live.com>
2021-09-24 14:09:38 +0900

    Introduce @Repeat annotation
    @Repeat annotation replaces repeat property of @AutoSource. Properties
    of annotation types are not extensible so repeat property is replaced
    with @Repeat annotation and will be removed in a future release.
    
    This resolves #272

---
+++ autoparams/src/test/java/org/javaunit/autoparams/test/SpecsForRepeat.java
@@ -0,0 +1,47 @@
+package org.javaunit.autoparams.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.javaunit.autoparams.AutoSource;
+import org.javaunit.autoparams.Repeat;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class SpecsForRepeat {
+
+    private static int shouldBe10 = 0;
+    private static int shouldBe3 = 0;
+
+    @ParameterizedTest
+    @AutoSource
+    @Repeat(10)
+    @Order(1)
+    void sut_work_correctly(int x) {
+        shouldBe10++;
+    }
+
+    @Test
+    @Order(2)
+    void verify_sut_work_correctly() {
+        assertEquals(10, shouldBe10);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    @Repeat
+    @Order(3)
+    void sut_has_default_value() {
+        shouldBe3++;
+    }
+
+    @Test
+    @Order(4)
+    void verify_sut_has_default_value() {
+        assertEquals(3, shouldBe3);
+    }
+
+}
