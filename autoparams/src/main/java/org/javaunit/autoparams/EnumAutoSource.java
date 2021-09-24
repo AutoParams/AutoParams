a12c7cfee8cfeb2396300ae3aeac1ff106286960
Gyuwon Yi <gyuwon@live.com>
2021-09-24 21:39:21 +0900

    Introduce @EnumAutoSource annotation
    @EnumAutoSource combines functionalities of @EnumSource and @AutoSource
    annotations.
    
    This resolves #268

---
+++ autoparams/src/main/java/org/javaunit/autoparams/EnumAutoSource.java
@@ -0,0 +1,21 @@
+package org.javaunit.autoparams;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.EnumSource;
+
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ArgumentsSource(EnumAutoArgumentsProvider.class)
+public @interface EnumAutoSource {
+
+    Class<? extends Enum<?>> value();
+
+    String[] names() default {};
+
+    EnumSource.Mode mode() default EnumSource.Mode.INCLUDE;
+
+}
