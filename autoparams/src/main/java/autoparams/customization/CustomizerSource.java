68c6846f30acde8955a1ee863c7fc2fab720e704
Gyuwon <gyuwon@live.com>
2023-09-17 01:56:38 +0900

    Define annotation CustomizerSource
    CustomizerSource specifies type of CustomizerFactory

---
+++ autoparams/src/main/java/autoparams/customization/CustomizerSource.java
@@ -0,0 +1,13 @@
+package autoparams.customization;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CustomizerSource {
+
+    Class<? extends CustomizerFactory> value();
+}
