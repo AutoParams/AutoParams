695846b80ba4db293ec3d366b58195b623520393
Gyuwon <gyuwon@live.com>
2023-09-16 20:06:41 +0900

    Add ConstructorResolver.resolveOrElseThrow method

--- autoparams/src/main/java/autoparams/generator/ConstructorResolver.java
+++ autoparams/src/main/java/autoparams/generator/ConstructorResolver.java
@@ -9,6 +9,11 @@ public interface ConstructorResolver {
 
     Optional<Constructor<?>> resolve(Class<?> type);
 
+    default Constructor<?> resolveOrElseThrow(Class<?> type) {
+        return resolve(type).orElseThrow(()
+            -> new RuntimeException("No constructor found for " + type));
+    }
+
     static ConstructorResolver compose(ConstructorResolver... resolvers) {
         return type -> Folder.foldl(
             (resolved, resolver) -> resolved.isPresent() ? resolved : resolver.resolve(type),
---
+++ autoparams/src/test/java/autoparams/generator/test/SpecsForConstructorResolver.java
@@ -0,0 +1,39 @@
+package autoparams.generator.test;
+
+import autoparams.generator.ConstructorResolver;
+import java.lang.reflect.Constructor;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SpecsForConstructorResolver {
+
+    @Test
+    void resolveOrElseThrow_returns_constructor_if_it_is_present() {
+        // Arrange
+        Class<String> type = String.class;
+        Constructor<?> constructor = type.getConstructors()[0];
+        ConstructorResolver sut = t -> t.equals(type)
+            ? Optional.of(constructor)
+            : Optional.empty();
+
+        // Act
+        Constructor<?> actual = sut.resolveOrElseThrow(type);
+
+        // Assert
+        assertThat(actual).isSameAs(constructor);
+    }
+
+    @Test
+    void resolveOrElseThrow_throws_exception_if_constructor_is_not_present() {
+        // Arrange
+        ConstructorResolver sut = t -> Optional.empty();
+
+        // Act & Assert
+        assertThatThrownBy(() -> sut.resolveOrElseThrow(String.class))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("No constructor found for " + String.class);
+    }
+}
