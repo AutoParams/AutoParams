34f61b15d5c4cb5a1d9d1dab68f096133ad00f97
Gyuwon Yi <gyuwon@live.com>
2024-02-16 16:00:34 +0900

    Add customize default method to Customizer
    Add customize default method to Customizer to customize ObjectProcessor.

--- autoparams/src/main/java/autoparams/customization/Customizer.java
+++ autoparams/src/main/java/autoparams/customization/Customizer.java
@@ -1,9 +1,14 @@
 package autoparams.customization;
 
 import autoparams.generator.ObjectGenerator;
+import autoparams.processor.ObjectProcessor;
 
 @FunctionalInterface
 public interface Customizer {
 
     ObjectGenerator customize(ObjectGenerator generator);
+
+    default ObjectProcessor customize(ObjectProcessor processor) {
+        return processor;
+    }
 }
