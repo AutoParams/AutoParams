c0afda5dc5fdf12ea1751f0f26ad09b5713e5ac1
Gyuwon Yi <gyuwon@live.com>
2024-03-30 22:24:42 +0900

    Add toCustomizer method to ObjectProcessor

--- autoparams/src/main/java/autoparams/processor/ObjectProcessor.java
+++ autoparams/src/main/java/autoparams/processor/ObjectProcessor.java
@@ -1,6 +1,8 @@
 package autoparams.processor;
 
 import autoparams.ResolutionContext;
+import autoparams.customization.Customizer;
+import autoparams.customization.DelegatingCustomizer;
 import autoparams.generator.ObjectQuery;
 
 @FunctionalInterface
@@ -9,4 +11,11 @@ public interface ObjectProcessor {
     void process(ObjectQuery query, Object value, ResolutionContext context);
 
     ObjectProcessor DEFAULT = (query, value, context) -> { };
+
+    default Customizer toCustomizer() {
+        return new DelegatingCustomizer(
+            generator -> generator,
+            processor -> new CompositeObjectProcessor(this, processor)
+        );
+    }
 }
---
+++ autoparams/src/test/java/test/autoparams/processor/SpecsForObjectProcessor.java
@@ -0,0 +1,30 @@
+package test.autoparams.processor;
+
+import autoparams.ResolutionContext;
+import autoparams.customization.Customizer;
+import autoparams.processor.InstancePropertyWriter;
+import test.autoparams.AutoParameterizedTest;
+import test.autoparams.HasSetter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SpecsForObjectProcessor {
+
+    @AutoParameterizedTest
+    void toCustomizer_returns_customizer(InstancePropertyWriter sut) {
+        Customizer actual = sut.toCustomizer();
+        assertThat(actual).isNotNull();
+    }
+
+    @AutoParameterizedTest
+    void toCustomizer_returns_customizer_that_works_correctly(
+        InstancePropertyWriter sut,
+        ResolutionContext context
+    ) {
+        Customizer actual = sut.toCustomizer();
+
+        context.applyCustomizer(actual);
+        HasSetter value = context.generate(HasSetter.class);
+        assertThat(value.getValue()).isNotNull();
+    }
+}
