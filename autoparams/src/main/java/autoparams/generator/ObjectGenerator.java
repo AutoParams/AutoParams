7016a318d58fd8f9e9a8a5dd97c0840d6081236b
Gyuwon Yi <gyuwon@live.com>
2024-03-30 22:16:07 +0900

    Add toCustomizer method to ObjectGenerator

--- autoparams/src/main/java/autoparams/generator/ObjectGenerator.java
+++ autoparams/src/main/java/autoparams/generator/ObjectGenerator.java
@@ -3,6 +3,8 @@ package autoparams.generator;
 import java.lang.reflect.Type;
 
 import autoparams.ResolutionContext;
+import autoparams.customization.Customizer;
+import autoparams.customization.DelegatingCustomizer;
 
 @FunctionalInterface
 public interface ObjectGenerator {
@@ -13,5 +15,12 @@ public interface ObjectGenerator {
 
     default ObjectContainer generate(Type type, ResolutionContext context) {
         return generate(ObjectQuery.fromType(type), context);
+    }
+
+    default Customizer toCustomizer() {
+        return new DelegatingCustomizer(
+            generator -> new CompositeObjectGenerator(this, generator),
+            processor -> processor
+        );
     }
 }
--- autoparams/src/test/java/test/autoparams/generator/SpecsForObjectGenerator.java
+++ autoparams/src/test/java/test/autoparams/generator/SpecsForObjectGenerator.java
@@ -1,13 +1,21 @@
 package test.autoparams.generator;
 
 import autoparams.ResolutionContext;
+import autoparams.customization.Customizer;
 import autoparams.generator.ObjectContainer;
 import autoparams.generator.ObjectGenerator;
+import autoparams.generator.TypeMatchingGenerator;
 import test.autoparams.AutoParameterizedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SpecsForObjectGenerator {
+
+    @FunctionalInterface
+    public interface StringFactory {
+
+        String get();
+    }
 
     @AutoParameterizedTest
     void generate_with_type_correctly_generates_value_of_the_type(
@@ -19,5 +27,34 @@ public class SpecsForObjectGenerator {
 
         assertThat(actual).isNotNull();
         assertThat(actual.unwrapOrElseThrow()).isInstanceOf(String.class);
+    }
+
+    @AutoParameterizedTest
+    void toCustomizer_returns_customizer(String value) {
+        ObjectGenerator sut = TypeMatchingGenerator.create(
+            StringFactory.class,
+            () -> () -> value
+        );
+
+        Customizer actual = sut.toCustomizer();
+
+        assertThat(actual).isNotNull();
+    }
+
+    @AutoParameterizedTest
+    void toCustomizer_returns_customizer_that_works_correctly(
+        ResolutionContext context,
+        String value
+    ) {
+        ObjectGenerator sut = TypeMatchingGenerator.create(
+            StringFactory.class,
+            () -> () -> value
+        );
+
+        Customizer actual = sut.toCustomizer();
+
+        context.applyCustomizer(actual);
+        StringFactory factory = context.generate(StringFactory.class);
+        assertThat(factory.get()).isEqualTo(value);
     }
 }
