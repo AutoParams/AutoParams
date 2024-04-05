7f09cb7ef0313ac48dbde011f97e026d0fabc393
Gyuwon Yi <gyuwon@live.com>
2024-04-05 13:01:55 +0900

    Add resolve extension method for ResolutionContext
    generate extension method is deprecated.

--- autoparams-kotlin/src/main/kotlin/autoparams/kotlin/ResolutionContextExtensions.kt
+++ autoparams-kotlin/src/main/kotlin/autoparams/kotlin/ResolutionContextExtensions.kt
@@ -2,6 +2,15 @@ package autoparams.kotlin
 
 import autoparams.ResolutionContext
 
+@Suppress("DEPRECATION")
+@Deprecated(
+    "Use ResolutionContext.resolve extension method instead",
+    ReplaceWith("resolve", "autoparams.kotlin.resolve")
+)
 inline fun <reified T> ResolutionContext.generate(): T {
     return this.generate { T::class.java } as T
+}
+
+inline fun <reified T> ResolutionContext.resolve(): T {
+    return this.resolve { T::class.java } as T
 }
--- autoparams-kotlin/src/test/kotlin/test/autoparams/kotlin/SpecsForResolutionContextExtensions.kt
+++ autoparams-kotlin/src/test/kotlin/test/autoparams/kotlin/SpecsForResolutionContextExtensions.kt
@@ -4,12 +4,14 @@ import autoparams.ResolutionContext
 import autoparams.generator.ObjectContainer
 import autoparams.kotlin.AutoKotlinSource
 import autoparams.kotlin.generate
+import autoparams.kotlin.resolve
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 
 class SpecsForResolutionContextExtensions {
 
+    @Suppress("DEPRECATION")
     @ParameterizedTest
     @AutoKotlinSource
     fun `generate correctly returns value of reified type`(
@@ -23,6 +25,23 @@ class SpecsForResolutionContextExtensions {
         )
 
         val actual: Int = sut.generate()
+
+        assertThat(actual).isEqualTo(value)
+    }
+
+    @ParameterizedTest
+    @AutoKotlinSource
+    fun `resolve correctly returns value of reified type`(
+        extensionContext: ExtensionContext,
+        value: Int,
+    ) {
+        val sut = ResolutionContext(
+            extensionContext,
+            { _, _ -> ObjectContainer(value) },
+            { _, _, _ -> }
+        )
+
+        val actual: Int = sut.resolve()
 
         assertThat(actual).isEqualTo(value)
     }
