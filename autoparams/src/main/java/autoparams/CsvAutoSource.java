48dcc3385fc3f32e6c75cb0b770d4472352ed344
Gyuwon Yi <gyuwon@live.com>
2024-04-06 09:51:33 +0900

    Add class ProxyFactory to CsvAutoSource

--- autoparams/src/main/java/autoparams/CsvAutoSource.java
+++ autoparams/src/main/java/autoparams/CsvAutoSource.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Proxy;
 
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
@@ -31,4 +32,42 @@ public @interface CsvAutoSource {
     int maxCharsPerColumn() default 4096;
 
     boolean ignoreLeadingAndTrailingWhitespace() default true;
+
+    class ProxyFactory {
+
+        public static CsvAutoSource create(
+            String[] value,
+            String textBlock,
+            boolean useHeadersInDisplayName,
+            char quoteCharacter,
+            char delimiter,
+            String delimiterString,
+            String emptyValue,
+            String[] nullValues,
+            int maxCharsPerColumn,
+            boolean ignoreLeadingAndTrailingWhitespace
+        ) {
+            return (CsvAutoSource) Proxy.newProxyInstance(
+                CsvAutoSource.class.getClassLoader(),
+                new Class[] { CsvAutoSource.class },
+                (proxy, method, args) -> {
+                    switch (method.getName()) {
+                        case "annotationType": return CsvAutoSource.class;
+                        case "value": return value;
+                        case "textBlock": return textBlock;
+                        case "useHeadersInDisplayName": return useHeadersInDisplayName;
+                        case "quoteCharacter": return quoteCharacter;
+                        case "delimiter": return delimiter;
+                        case "delimiterString": return delimiterString;
+                        case "emptyValue": return emptyValue;
+                        case "nullValues": return nullValues;
+                        case "maxCharsPerColumn": return maxCharsPerColumn;
+                        case "ignoreLeadingAndTrailingWhitespace":
+                            return ignoreLeadingAndTrailingWhitespace;
+                        default: return method.getDefaultValue();
+                    }
+                }
+            );
+        }
+    }
 }
--- autoparams/src/test/java/test/autoparams/SpecsForCsvAutoSource.java
+++ autoparams/src/test/java/test/autoparams/SpecsForCsvAutoSource.java
@@ -2,6 +2,7 @@ package test.autoparams;
 
 import java.util.UUID;
 
+import autoparams.AutoSource;
 import autoparams.CsvAutoSource;
 import autoparams.customization.Fix;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -53,5 +54,107 @@ class SpecsForCsvAutoSource {
     ) {
         assertEquals(16, value3.getValue1());
         assertEquals("foo", value3.getValue2());
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void proxy_factory_creates_instance(
+        String[] value,
+        String textBlock,
+        boolean useHeadersInDisplayName,
+        char quoteCharacter,
+        char delimiter,
+        String delimiterString,
+        String emptyValue,
+        String[] nullValues,
+        int maxCharsPerColumn,
+        boolean ignoreLeadingAndTrailingWhitespace
+    ) {
+        CsvAutoSource actual = CsvAutoSource.ProxyFactory.create(
+            value,
+            textBlock,
+            useHeadersInDisplayName,
+            quoteCharacter,
+            delimiter,
+            delimiterString,
+            emptyValue,
+            nullValues,
+            maxCharsPerColumn,
+            ignoreLeadingAndTrailingWhitespace
+        );
+
+        assertNotNull(actual);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void proxy_factory_correctly_configures_annotationType(
+        String[] value,
+        String textBlock,
+        boolean useHeadersInDisplayName,
+        char quoteCharacter,
+        char delimiter,
+        String delimiterString,
+        String emptyValue,
+        String[] nullValues,
+        int maxCharsPerColumn,
+        boolean ignoreLeadingAndTrailingWhitespace
+    ) {
+        CsvAutoSource actual = CsvAutoSource.ProxyFactory.create(
+            value,
+            textBlock,
+            useHeadersInDisplayName,
+            quoteCharacter,
+            delimiter,
+            delimiterString,
+            emptyValue,
+            nullValues,
+            maxCharsPerColumn,
+            ignoreLeadingAndTrailingWhitespace
+        );
+
+        assertEquals(CsvAutoSource.class, actual.annotationType());
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void proxy_factory_correctly_configures_properties(
+        String[] value,
+        String textBlock,
+        boolean useHeadersInDisplayName,
+        char quoteCharacter,
+        char delimiter,
+        String delimiterString,
+        String emptyValue,
+        String[] nullValues,
+        int maxCharsPerColumn,
+        boolean ignoreLeadingAndTrailingWhitespace
+    ) {
+        CsvAutoSource actual = CsvAutoSource.ProxyFactory.create(
+            value,
+            textBlock,
+            useHeadersInDisplayName,
+            quoteCharacter,
+            delimiter,
+            delimiterString,
+            emptyValue,
+            nullValues,
+            maxCharsPerColumn,
+            ignoreLeadingAndTrailingWhitespace
+        );
+
+        assertEquals(value, actual.value());
+        assertEquals(textBlock, actual.textBlock());
+        assertEquals(useHeadersInDisplayName, actual.useHeadersInDisplayName());
+        assertEquals(quoteCharacter, actual.quoteCharacter());
+        assertEquals(delimiter, actual.delimiter());
+        assertEquals(delimiterString, actual.delimiterString());
+        assertEquals(emptyValue, actual.emptyValue());
+        assertEquals(nullValues, actual.nullValues());
+        assertEquals(maxCharsPerColumn, actual.maxCharsPerColumn());
+        assertEquals(
+            ignoreLeadingAndTrailingWhitespace,
+            actual.ignoreLeadingAndTrailingWhitespace()
+        );
     }
 }
