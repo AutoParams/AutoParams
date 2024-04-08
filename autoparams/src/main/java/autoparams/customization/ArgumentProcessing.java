00b08903701db6978dde3a6497deecfdc991ca64
Gyuwon Yi <gyuwon@live.com>
2024-04-08 19:08:14 +0900

    Change target of @ArgumentProcessing

--- autoparams/src/main/java/autoparams/customization/ArgumentProcessing.java
+++ autoparams/src/main/java/autoparams/customization/ArgumentProcessing.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE})
+@Target({ ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ArgumentProcessing {
 
