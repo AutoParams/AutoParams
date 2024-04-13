6d55ceacae8eaf3e830aad3a2bd8306e47974858
Gyuwon Yi <gyuwon@live.com>
2024-04-14 06:08:52 +0900

    Refactor Enum generation

--- autoparams/src/main/java/autoparams/generator/EnumGenerator.java
+++ autoparams/src/main/java/autoparams/generator/EnumGenerator.java
@@ -1,24 +1,44 @@
 package autoparams.generator;
 
 import java.lang.reflect.Type;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 import autoparams.ResolutionContext;
 
 final class EnumGenerator implements ObjectGenerator {
 
+    private final Map<Class<?>, Object[]> cache = new ConcurrentHashMap<>();
+
     @Override
-    public ObjectContainer generate(ObjectQuery query, ResolutionContext context) {
+    public ObjectContainer generate(
+        ObjectQuery query,
+        ResolutionContext context
+    ) {
         Type type = query.getType();
-        return type instanceof Class && Enum.class.isAssignableFrom((Class<?>) type)
-            ? new ObjectContainer(factory(query))
+        return isEnum(type)
+            ? new ObjectContainer(generateValue((Class<?>) type))
             : ObjectContainer.EMPTY;
     }
 
-    private Object factory(ObjectQuery query) {
-        Class<?> enumType = ((Class<?>) query.getType());
-        Object[] values = EnumValuesResolver.resolveValues(enumType);
+    private static boolean isEnum(Type type) {
+        return type instanceof Class
+            && Enum.class.isAssignableFrom((Class<?>) type);
+    }
+
+    private Object generateValue(Class<?> enumType) {
+        Object[] values = getValues(enumType);
         int index = ThreadLocalRandom.current().nextInt(values.length);
         return values[index];
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public Object[] getValues(Class<?> type) {
+        return cache.computeIfAbsent(
+            type,
+            t -> EnumSet.allOf((Class<Enum>) t).toArray()
+        );
     }
 }
