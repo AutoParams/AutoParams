717598720675c743c296a127cacb7cbeec5226f3
Gyuwon Yi <gyuwon@live.com>
2024-04-16 00:17:27 +0900

    Refactor ArgumentsProviderCreator

--- autoparams/src/main/java/autoparams/ArgumentsProviderCreator.java
+++ autoparams/src/main/java/autoparams/ArgumentsProviderCreator.java
@@ -1,40 +1,13 @@
 package autoparams;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 final class ArgumentsProviderCreator {
 
-    public static ArgumentsProvider createArgumentsProvider(
-        Class<? extends Annotation> source
-    ) {
-        Class<? extends ArgumentsProvider> provider = source
-            .getAnnotation(ArgumentsSource.class)
-            .value();
-        return createInstance(provider);
-    }
-
-    private static <T> T createInstance(Class<? extends T> type) {
-        try {
-            return getConstructor(type).newInstance();
-        } catch (NoSuchMethodException |
-                 InstantiationException |
-                 IllegalAccessException |
-                 InvocationTargetException exception
-        ) {
-            throw new RuntimeException(exception);
-        }
-    }
-
-    private static <T> Constructor<? extends T> getConstructor(
-        Class<? extends T> type
-    ) throws NoSuchMethodException {
-        Constructor<? extends T> constructor = type.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        return constructor;
+    public static ArgumentsProvider createArgumentsProvider(Class<?> entry) {
+        ArgumentsSource source = entry.getAnnotation(ArgumentsSource.class);
+        Class<? extends ArgumentsProvider> provider = source.value();
+        return Instantiator.instantiate(provider);
     }
 }
