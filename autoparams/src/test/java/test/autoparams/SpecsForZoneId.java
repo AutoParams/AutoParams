7fcae8c79e693c215c0bfa989ec874b548cf17ef
Gyuwon Yi <gyuwon@live.com>
2024-04-26 16:42:44 +0900

    Fix false positive of ZoneId generation test

--- autoparams/src/test/java/test/autoparams/SpecsForZoneId.java
+++ autoparams/src/test/java/test/autoparams/SpecsForZoneId.java
@@ -1,6 +1,8 @@
 package test.autoparams;
 
 import java.time.ZoneId;
+import java.util.HashSet;
+import java.util.Set;
 
 import autoparams.AutoSource;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,10 +23,16 @@ public class SpecsForZoneId {
     void sut_creates_anonymous_ZoneId_value(
         ZoneId value1,
         ZoneId value2,
-        ZoneId value3
+        ZoneId value3,
+        ZoneId value4,
+        ZoneId value5
     ) {
-        assertNotEquals(value1, value2);
-        assertNotEquals(value2, value3);
-        assertNotEquals(value3, value1);
+        Set<ZoneId> set = new HashSet<>();
+        set.add(value1);
+        set.add(value2);
+        set.add(value3);
+        set.add(value4);
+        set.add(value5);
+        assertNotEquals(1, set.size());
     }
 }
