6194e5b0b049e8e5fa2a7ef089e0d1f91234dbea
Gyuwon Yi <gyuwon@live.com>
2024-05-02 17:32:53 +0900

    Supports string properties of uri container

--- autoparams/src/main/java/autoparams/generator/URIStringGenerator.java
+++ autoparams/src/main/java/autoparams/generator/URIStringGenerator.java
@@ -1,15 +1,22 @@
 package autoparams.generator;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Executable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.net.URI;
-import java.util.stream.Stream;
 
 import autoparams.ObjectQuery;
 import autoparams.ParameterQuery;
 import autoparams.ResolutionContext;
 
+import static java.util.Arrays.stream;
+
 final class URIStringGenerator implements ObjectGenerator {
 
-    private static final String[] SUFFIXES = { "uri", "url" };
+    private static final String[] SINGULAR_SUFFIXES = { "uri", "url" };
+    private static final String[] PLURAL_SUFFIXES = { "uris", "urls" };
 
     @Override
     public ObjectContainer generate(
@@ -26,11 +33,43 @@ final class URIStringGenerator implements ObjectGenerator {
         ParameterQuery query,
         ResolutionContext context
     ) {
+        return isUriParameter(query) || isPropertyOfUriContainer(query)
+            ? new ObjectContainer(context.resolve(URI.class).toString())
+            : ObjectContainer.EMPTY;
+    }
+
+    private boolean isUriParameter(ParameterQuery query) {
         return query.getParameterName()
             .map(String::toLowerCase)
-            .filter(name -> Stream.of(SUFFIXES).anyMatch(name::endsWith))
-            .map(name -> context.resolve(URI.class).toString())
-            .map(ObjectContainer::new)
-            .orElse(ObjectContainer.EMPTY);
+            .filter(name -> stream(SINGULAR_SUFFIXES).anyMatch(name::endsWith))
+            .isPresent();
+    }
+
+    private boolean isPropertyOfUriContainer(ParameterQuery query) {
+        return stream(PLURAL_SUFFIXES).anyMatch(suffix -> {
+            Parameter parameter = query.getParameter();
+            Executable executable = parameter.getDeclaringExecutable();
+            return isRecordConstructor(executable)
+                && executable.getName().toLowerCase().endsWith(suffix);
+        });
+    }
+
+    private boolean isRecordConstructor(Executable executable) {
+        return executable instanceof Constructor<?>
+            && isRecord(executable.getDeclaringClass());
+    }
+
+    private boolean isRecord(Class<?> type) {
+        try {
+            Method predicate = Class.class.getMethod("isRecord");
+            try {
+                return (boolean) predicate.invoke(type);
+            } catch (IllegalAccessException |
+                     InvocationTargetException exception) {
+                throw new RuntimeException(exception);
+            }
+        } catch (NoSuchMethodException exception) {
+            return false;
+        }
     }
 }
--- test-autoparams-java-17/src/test/java/test/autoparams/SpecsForURIStringProperty.java
+++ test-autoparams-java-17/src/test/java/test/autoparams/SpecsForURIStringProperty.java
@@ -22,4 +22,48 @@ public class SpecsForURIStringProperty {
         String regex = "^(http|https|ftp)://[^\\s/$.?#].\\S*$";
         assertThat(properties.videoUrl().matches(regex)).isTrue();
     }
+
+    public record ImageUris(String thumbnail, String fullSize) { }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_generates_uri_string_for_string_properties_of_type_with_uris_suffix(
+        ImageUris uris
+    ) {
+        String regex = "^(http|https|ftp)://[^\\s/$.?#].\\S*$";
+        assertThat(uris.thumbnail().matches(regex)).isTrue();
+        assertThat(uris.fullSize().matches(regex)).isTrue();
+    }
+
+    public record ImageUrls(String thumbnail, String fullSize) { }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_generates_uri_string_for_string_properties_of_type_with_urls_suffix(
+        ImageUrls urls
+    ) {
+        String regex = "^(http|https|ftp)://[^\\s/$.?#].\\S*$";
+        assertThat(urls.thumbnail().matches(regex)).isTrue();
+        assertThat(urls.fullSize().matches(regex)).isTrue();
+    }
+
+    @SuppressWarnings("ClassCanBeRecord")
+    public static class PictureUris {
+
+        public final String thumbnail;
+        public final String fullSize;
+
+        public PictureUris(String thumbnail, String fullSize) {
+            this.thumbnail = thumbnail;
+            this.fullSize = fullSize;
+        }
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void sut_does_not_support_properties_of_non_record_type(PictureUris uris) {
+        String regex = "^(http|https|ftp)://[^\\s/$.?#].\\S*$";
+        assertThat(uris.thumbnail.matches(regex)).isFalse();
+        assertThat(uris.fullSize.matches(regex)).isFalse();
+    }
 }
