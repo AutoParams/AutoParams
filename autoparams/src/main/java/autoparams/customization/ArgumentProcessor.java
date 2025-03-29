739cb144ae4ba05745121028785b7fef1b3672b3
Gyuwon Yi <gyuwon@live.com>
2025-03-30 02:30:54 +0900

    Delete deprecated ArgumentProcessor interface

--- autoparams/src/main/java/autoparams/customization/ArgumentProcessor.java
+++
@@ -1,9 +0,0 @@
-package autoparams.customization;
-
-import java.lang.reflect.Parameter;
-
-@Deprecated
-public interface ArgumentProcessor {
-
-    Customizer process(Parameter parameter, Object argument);
-}
