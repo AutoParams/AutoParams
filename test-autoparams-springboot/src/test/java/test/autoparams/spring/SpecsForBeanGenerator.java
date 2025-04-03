c2c1997c0522c42f7ed72c2c0957ad0dfc77728f
Gyuwon Yi <gyuwon@live.com>
2025-04-04 03:56:33 +0900

    Remove unused import

--- test-autoparams-springboot/src/test/java/test/autoparams/spring/SpecsForBeanGenerator.java
+++ test-autoparams-springboot/src/test/java/test/autoparams/spring/SpecsForBeanGenerator.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.ApplicationContext;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
