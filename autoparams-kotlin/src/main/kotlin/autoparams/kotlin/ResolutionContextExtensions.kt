fe925170913a43e451c321137b3b2e7388559a8b
Gyuwon Yi <gyuwon@live.com>
2025-04-04 11:30:33 +0900

    Delete unused reified generate function

--- autoparams-kotlin/src/main/kotlin/autoparams/kotlin/ResolutionContextExtensions.kt
+++ autoparams-kotlin/src/main/kotlin/autoparams/kotlin/ResolutionContextExtensions.kt
@@ -2,14 +2,6 @@ package autoparams.kotlin
 
 import autoparams.ResolutionContext
 
-@Deprecated(
-    "Use ResolutionContext.resolve extension method instead",
-    ReplaceWith("resolve", "autoparams.kotlin.resolve")
-)
-inline fun <reified T> ResolutionContext.generate(): T {
-    return this.resolve { T::class.java } as T
-}
-
 inline fun <reified T> ResolutionContext.resolve(): T {
     return this.resolve { T::class.java } as T
 }
