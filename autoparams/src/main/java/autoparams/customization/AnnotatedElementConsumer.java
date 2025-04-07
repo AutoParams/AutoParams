d1aa4569713260993e3e2224d99853e6c774f300
Gyuwon Yi <gyuwon@live.com>
2025-04-07 10:54:16 +0900

    Make AnnotatedElementConsumer functional

--- autoparams/src/main/java/autoparams/customization/AnnotatedElementConsumer.java
+++ autoparams/src/main/java/autoparams/customization/AnnotatedElementConsumer.java
@@ -3,5 +3,6 @@ package autoparams.customization;
 import java.lang.reflect.AnnotatedElement;
 import java.util.function.Consumer;
 
+@FunctionalInterface
 public interface AnnotatedElementConsumer extends Consumer<AnnotatedElement> {
 }
