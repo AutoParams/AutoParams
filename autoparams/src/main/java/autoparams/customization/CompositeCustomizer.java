2f17a0a57cf2a48a1f2490bee3527da7419e4407
Gyuwon Yi <gyuwon@live.com>
2025-04-09 02:24:32 +0900

    Refactor CompositeCustomizer class

--- autoparams/src/main/java/autoparams/customization/CompositeCustomizer.java
+++ autoparams/src/main/java/autoparams/customization/CompositeCustomizer.java
@@ -1,12 +1,9 @@
 package autoparams.customization;
 
-import java.util.Iterator;
-import java.util.function.BiFunction;
-import java.util.stream.Stream;
-
 import autoparams.generator.ObjectGenerator;
 import autoparams.processor.ObjectProcessor;
 
+import static autoparams.internal.Folder.foldl;
 import static java.util.Arrays.stream;
 
 public class CompositeCustomizer implements Customizer {
@@ -25,14 +22,5 @@ public class CompositeCustomizer implements Customizer {
     @Override
     public ObjectProcessor customize(ObjectProcessor processor) {
         return foldl((p, c) -> c.customize(p), processor, stream(customizers));
-    }
-
-    private static <T, U> U foldl(BiFunction<U, T, U> f, U z, Stream<T> xs) {
-        Iterator<T> i = xs.iterator();
-        U a = z;
-        while (i.hasNext()) {
-            a = f.apply(a, i.next());
-        }
-        return a;
     }
 }
