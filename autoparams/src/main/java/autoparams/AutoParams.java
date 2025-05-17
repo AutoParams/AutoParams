c6889ebb229173e98cd47ef3cf7471c7553581bd
Gyuwon Yi <gyuwon@live.com>
2025-05-18 00:07:22 +0900

    Add Javadoc to @AutoParams

--- autoparams/src/main/java/autoparams/AutoParams.java
+++ autoparams/src/main/java/autoparams/AutoParams.java
@@ -7,6 +7,21 @@ import java.lang.annotation.Target;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 
+/**
+ * Annotation to enable automatic parameter injection for JUnit 5 test methods.
+ * <p>
+ * When this annotation is applied to a test method, {@link AutoParamsExtension}
+ * automatically provides suitable values for the method parameters.
+ * </p>
+ *
+ * <pre>
+ * &#64;Test
+ * &#64;AutoParams
+ * void testMethod(String arg1, UUID arg2) {
+ *     // arg1 and arg2 are automatically generated and injected.
+ * }
+ * </pre>
+ */
 @Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(AutoParamsExtension.class)
