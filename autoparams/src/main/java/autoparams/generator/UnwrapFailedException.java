f7f518362456f7563e4ffbfe9e0508211fd0d74c
Gyuwon Yi <gyuwon@live.com>
2025-05-18 00:47:15 +0900

    Add Javadoc to UnwrapFailedException

--- autoparams/src/main/java/autoparams/generator/UnwrapFailedException.java
+++ autoparams/src/main/java/autoparams/generator/UnwrapFailedException.java
@@ -1,5 +1,14 @@
 package autoparams.generator;
 
+/**
+ * Exception thrown when attempting to unwrap an empty {@link ObjectContainer}.
+ * <p>
+ * This exception indicates that the container does not hold a value and
+ * an unwrap operation was attempted, which is not allowed.
+ * </p>
+ *
+ * @see ObjectContainer#unwrapOrElseThrow()
+ */
 public final class UnwrapFailedException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
