f7dfa79b242a14ddf992cf6883d05d7046e21aee
Gyuwon Yi <gyuwon@live.com>
2025-05-18 01:35:19 +0900

    Add Javadoc to ObjectQuery

--- autoparams/src/main/java/autoparams/ObjectQuery.java
+++ autoparams/src/main/java/autoparams/ObjectQuery.java
@@ -2,8 +2,24 @@ package autoparams;
 
 import java.lang.reflect.Type;
 
+/**
+ * Represents a query for an object of a specific {@link Type}.
+ * <p>
+ * Implementations of this interface provide type information used for object
+ * generation in the AutoParams framework.
+ * </p>
+ *
+ * @see DefaultObjectQuery
+ * @see ParameterQuery
+ * @see FieldQuery
+ */
 @FunctionalInterface
 public interface ObjectQuery {
 
+    /**
+     * Returns the {@link Type} that this query represents.
+     *
+     * @return the type information for object generation
+     */
     Type getType();
 }
