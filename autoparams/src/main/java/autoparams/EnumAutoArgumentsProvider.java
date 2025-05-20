e1225a4790788143b888bda557398c4544fbc762
Gyuwon Yi <gyuwon@live.com>
2025-05-21 01:05:17 +0900

    Add Javadoc to EnumAutoArgumentsProvider

--- autoparams/src/main/java/autoparams/EnumAutoArgumentsProvider.java
+++ autoparams/src/main/java/autoparams/EnumAutoArgumentsProvider.java
@@ -8,12 +8,45 @@ import org.junit.jupiter.params.support.AnnotationConsumer;
 
 import static autoparams.ArgumentsProviderCreator.createArgumentsProvider;
 
+/**
+ * An implementation of {@link ArgumentsProvider} that processes
+ * {@link EnumAutoSource} annotations and combines enum constants with
+ * automatic parameter generation.
+ * <p>
+ * This provider acts as a bridge between JUnit 5's standard {@link EnumSource}
+ * functionality and AutoParams' automatic test data generation. It enables the
+ * use of enum constants for the first parameter while automatically generating
+ * values for any remaining parameters.
+ * </p>
+ *
+ * <p>
+ * The provider works by:
+ * </p>
+ * <ol>
+ *   <li>Processing the {@link EnumAutoSource} annotation</li>
+ *   <li>
+ *       Converting it to an {@link EnumSource} for JUnit to handle enum
+ *       constant selection
+ *   </li>
+ *   <li>
+ *       Delegating to {@link AutoArgumentsProvider} to fill in missing
+ *       parameters
+ *   </li>
+ * </ol>
+ *
+ * @see EnumAutoSource
+ * @see AutoArgumentsProvider
+ * @see EnumSource
+ */
 public final class EnumAutoArgumentsProvider
     extends AutoArgumentsProvider
     implements AnnotationConsumer<EnumAutoSource> {
 
     private final AnnotationConsumer<EnumSource> annotationConsumer;
 
+    /**
+     * Constructs a new {@link EnumAutoArgumentsProvider} instance.
+     */
     @SuppressWarnings("unused")
     public EnumAutoArgumentsProvider() {
         this(createArgumentsProvider(EnumSource.class));
@@ -25,6 +58,18 @@ public final class EnumAutoArgumentsProvider
         annotationConsumer = (AnnotationConsumer<EnumSource>) assetProvider;
     }
 
+    /**
+     * Accepts and processes an {@link EnumAutoSource} annotation.
+     * <p>
+     * This method converts the {@link EnumAutoSource} annotation to an
+     * equivalent {@link EnumSource} annotation and passes it to the internal
+     * annotation consumer. This allows the enum constants to be properly
+     * selected while maintaining compatibility with AutoParams' automatic
+     * parameter generation.
+     * </p>
+     *
+     * @param annotation the {@link EnumAutoSource} annotation to process
+     */
     @Override
     public void accept(EnumAutoSource annotation) {
         annotationConsumer.accept((EnumSource) Proxy.newProxyInstance(
