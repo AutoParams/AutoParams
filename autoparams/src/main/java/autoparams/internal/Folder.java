e29a09a98270d7add157f0c640ecbf46f06a36fd
Gyuwon Yi <gyuwon@live.com>
2025-05-21 20:11:57 +0900

    Add Javadoc to Folder

--- autoparams/src/main/java/autoparams/internal/Folder.java
+++ autoparams/src/main/java/autoparams/internal/Folder.java
@@ -4,11 +4,39 @@ import java.util.Iterator;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
+/**
+ * Provides a utility method for folding operations for streams.
+ * <p>
+ * This class is not meant to be instantiated and contains only static utility
+ * methods.
+ * </p>
+ */
 public final class Folder {
 
     private Folder() {
     }
 
+    /**
+     * Performs a left-associative fold operation on a stream.
+     * <p>
+     * This method applies a binary operator to an initial value and all
+     * elements of the stream, going from left to right. For a stream
+     * {@code [x1, x2, ..., xn]}, the result is
+     * {@code f(...f(f(z, x1), x2)..., xn)}.
+     * </p>
+     *
+     * @param <T> the type of the elements in the stream.
+     * @param <U> the type of the result and the initial value.
+     * @param f   the binary operator to apply. This function takes the
+     *            current accumulated value and the next element from
+     *            the stream as arguments and returns the new accumulated
+     *            value.
+     * @param z   the initial value for the fold operation.
+     * @param xs  the stream of elements to fold.
+     * @return the result of the left fold operation.
+     * @see Stream
+     * @see BiFunction
+     */
     public static <T, U> U foldl(BiFunction<U, T, U> f, U z, Stream<T> xs) {
         Iterator<T> i = xs.iterator();
         U a = z;
