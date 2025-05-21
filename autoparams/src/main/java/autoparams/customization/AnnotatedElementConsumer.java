57c3770a0678a26fb9e4c62f9c093f3f39394fe8
Gyuwon Yi <gyuwon@live.com>
2025-05-21 13:42:25 +0900

    Add Javadoc to AnnotatedElementConsumer

--- autoparams/src/main/java/autoparams/customization/AnnotatedElementConsumer.java
+++ autoparams/src/main/java/autoparams/customization/AnnotatedElementConsumer.java
@@ -3,6 +3,19 @@ package autoparams.customization;
 import java.lang.reflect.AnnotatedElement;
 import java.util.function.Consumer;
 
+/**
+ * Represents an operation that accepts a single {@link AnnotatedElement}
+ * argument and returns no result.
+ * <p>
+ * This is a functional interface whose functional method is
+ * {@link Consumer#accept}. It is typically used in scenarios where an action
+ * needs to be performed on an annotated element, such as processing annotations
+ * or modifying behavior based on them.
+ * </p>
+ *
+ * @see AnnotatedElement
+ * @see Consumer
+ */
 @FunctionalInterface
 public interface AnnotatedElementConsumer extends Consumer<AnnotatedElement> {
 }
