c24d58a4c8ee1a817d71f3a2a120d07a69197bcd
Gyuwon Yi <gyuwon@live.com>
2025-05-21 20:32:48 +0900

    Add Javadoc to EmailAddressGenerationOptions

--- autoparams/src/main/java/autoparams/generator/EmailAddressGenerationOptions.java
+++ autoparams/src/main/java/autoparams/generator/EmailAddressGenerationOptions.java
@@ -5,13 +5,40 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableList;
 
+/**
+ * Represents options for generating email address strings.
+ * <p>
+ * This class allows specifying a list of domains that can be used when
+ * generating email address strings. Instances of this class are immutable.
+ * </p>
+ *
+ */
 public final class EmailAddressGenerationOptions {
 
+    /**
+     * Provides default options for generating email address strings.
+     * <p>
+     * This instance is configured with a single default domain: "test.com".
+     * </p>
+     */
     public static final EmailAddressGenerationOptions DEFAULT =
         new EmailAddressGenerationOptions(new String[] { "test.com" });
 
     private final List<String> domains;
 
+    /**
+     * Constructs an instance of {@link EmailAddressGenerationOptions} with the
+     * specified domains.
+     *
+     * @param domains an array of strings representing the list of domains that
+     *                can be used when generating email address strings. This
+     *                array must not be null or empty, and none of its elements
+     *                can be null.
+     * @throws IllegalArgumentException if {@code domains} is null, or if
+     *                                  {@code domains} is empty, or if
+     *                                  {@code domains} contains any null
+     *                                  elements.
+     */
     public EmailAddressGenerationOptions(String[] domains) {
         if (domains == null) {
             throw new IllegalArgumentException("The argument 'domains' is null.");
@@ -30,6 +57,12 @@ public final class EmailAddressGenerationOptions {
         this.domains = unmodifiableList(asList(domains));
     }
 
+    /**
+     * Gets the list of domains that can be used when generating email address
+     * strings.
+     *
+     * @return an unmodifiable list of domain strings.
+     */
     public List<String> domains() {
         return domains;
     }
