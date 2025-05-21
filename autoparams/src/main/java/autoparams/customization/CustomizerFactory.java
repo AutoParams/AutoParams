0e8a7665aadce55a2f60bf6aca38fb9f0298e5e3
Gyuwon Yi <gyuwon@live.com>
2025-05-21 12:56:03 +0900

    Add Javadoc to CustomizerFactory

--- autoparams/src/main/java/autoparams/customization/CustomizerFactory.java
+++ autoparams/src/main/java/autoparams/customization/CustomizerFactory.java
@@ -1,7 +1,33 @@
 package autoparams.customization;
 
+/**
+ * Factory interface for creating {@link Customizer} instances.
+ * <p>
+ * This functional interface defines a factory method for creating customizers
+ * that can modify or extend the behavior of object generation and processing
+ * in AutoParams.
+ * </p>
+ * <p>
+ * Implementations of this interface can be referenced by the
+ * {@link CustomizerSource} annotation to specify how customizers should be
+ * created for a particular context.
+ * </p>
+ *
+ * @see Customizer
+ * @see CustomizerSource
+ */
 @FunctionalInterface
 public interface CustomizerFactory {
 
+    /**
+     * Creates a new {@link Customizer} instance.
+     * <p>
+     * This method is responsible for instantiating and configuring a customizer
+     * that can be used to modify the behavior of object generation and
+     * processing in AutoParams.
+     * </p>
+     *
+     * @return a new customizer instance
+     */
     Customizer createCustomizer();
 }
