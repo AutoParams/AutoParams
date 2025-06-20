938190ada6fe27a59d7c7523ed5bf4d37d171ebc
Gyuwon Yi <gyuwon@live.com>
2025-06-20 17:24:02 +0900

    Update JUnit version for autoparams project
    Update to JUnit 5.11.0

--- autoparams/build.gradle.kts
+++ autoparams/build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 dependencies {
-    api("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    api("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    api("org.junit.jupiter:junit-jupiter-api:5.11.0")
+    api("org.junit.jupiter:junit-jupiter-params:5.11.0")
     api("jakarta.validation:jakarta.validation-api:3.0.2")
     api("com.google.code.findbugs:jsr305:3.0.2")
 
     testImplementation("org.assertj:assertj-core:3.8.0")
     testImplementation("org.mockito:mockito-core:5.14.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
 
     testCompileOnly("org.projectlombok:lombok:1.18.30")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
