37de1ea2a490b2fb56f327b541ba87be22d88b81
Gyuwon Yi <gyuwon@live.com>
2025-06-21 14:30:43 +0900

    Update autoparams dependency version to 11.1.0

--- autoparams-lombok/build.gradle.kts
+++ autoparams-lombok/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    api("io.github.autoparams:autoparams:[11.0.0, 12.0.0)")
+    api("io.github.autoparams:autoparams:[11.1.0, 12.0.0)")
     testImplementation("org.assertj:assertj-core:3.8.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 }
--- autoparams-mockito/build.gradle.kts
+++ autoparams-mockito/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    api("io.github.autoparams:autoparams:[11.0.0, 12.0.0)")
+    api("io.github.autoparams:autoparams:[11.1.0, 12.0.0)")
     api("org.mockito:mockito-core:5.14.2")
     testImplementation("org.assertj:assertj-core:3.8.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
