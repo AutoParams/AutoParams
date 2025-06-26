024415cc151349bcc467dadda811e657f6abe1f4
Gyuwon Yi <gyuwon@live.com>
2025-06-27 00:24:34 +0900

    Rename SpecsForReporting class
    Rename to SpecsForResolutionLogging

--- test-autoparams-java-17/src/test/java/test/autoparams/SpecsForResolutionLogging.java
+++ test-autoparams-java-17/src/test/java/test/autoparams/SpecsForResolutionLogging.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SpecsForReporting {
+public class SpecsForResolutionLogging {
 
     @Test
     @AutoParams
