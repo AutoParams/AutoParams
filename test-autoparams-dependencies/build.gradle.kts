42b7654b81db532ffd60a2681bc0ef988fc07811
Vader <vader@shoplive.cloud>
2025-07-10 11:14:21 +0900

    Test version 11.2.3 dependencies

--- test-autoparams-dependencies/build.gradle.kts
+++ test-autoparams-dependencies/build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
 }
 
 dependencies {
-    api("io.github.autoparams:autoparams:[11.2.1, 12.0.0)")
-    api("io.github.autoparams:autoparams-kotlin:[11.2.1, 12.0.0)")
-    api("io.github.autoparams:autoparams-lombok:[11.2.1, 12.0.0)")
-    api("io.github.autoparams:autoparams-mockito:[11.2.1, 12.0.0)")
+    api("io.github.autoparams:autoparams:[11.2.3, 12.0.0)")
+    api("io.github.autoparams:autoparams-kotlin:[11.2.3, 12.0.0)")
+    api("io.github.autoparams:autoparams-lombok:[11.2.3, 12.0.0)")
+    api("io.github.autoparams:autoparams-mockito:[11.2.3, 12.0.0)")
     testImplementation("org.assertj:assertj-core:3.8.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.2")
