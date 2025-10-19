e223ee6da00de231ff89d94aa065963f0c38f101
Minsu Lee <amond@amond.net>
2025-10-19 19:08:26 +0900

    Refactor BigRandom to improve encapsulation
    Adjusted method visibility to expose only the necessary API and fixed
    the logarithm calculation for improved accuracy.

--- autoparams/src/main/java/autoparams/generator/BigRandom.java
+++ autoparams/src/main/java/autoparams/generator/BigRandom.java
@@ -7,37 +7,32 @@ import java.util.concurrent.ThreadLocalRandom;
 
 class BigRandom {
 
-    // Constants:
-    private static final double log2 = Math.log10(2.0);
-
     // Computes number of bits needed to represent an n digit positive integer.
 
     private static int bitCount(int n) {
-        return (int) (n / log2);
+        return (int) Math.ceil(n * Math.log(10) / Math.log(2));
     }
 
-    public static BigDecimal between(BigDecimal min, BigDecimal max, Random r) {
+    static BigDecimal between(BigDecimal min, BigDecimal max) {
+        return between(min, max, ThreadLocalRandom.current());
+    }
+
+    private static BigDecimal between(BigDecimal min, BigDecimal max, Random r) {
         return min.add(
             nextBigDecimal(max.subtract(min), Math.max(min.precision(), max.precision()), r));
     }
 
-    public static BigDecimal between(BigDecimal min, BigDecimal max) {
-        return between(min, max, ThreadLocalRandom.current());
-    }
-
-    // Static Methods for generating Random BigDecimal values:
-
-    public static BigDecimal nextBigDecimal(int scale) {
+    private static BigDecimal nextBigDecimal(int scale) {
         return nextBigDecimal(scale, ThreadLocalRandom.current());
     }
 
-    public static BigDecimal nextBigDecimal(int scale, Random r) {
+    private static BigDecimal nextBigDecimal(int scale, Random r) {
         BigInteger bi = new BigInteger(bitCount(scale), r);
-        BigDecimal bd = new BigDecimal(bi);  // convert BigInteger to a BigDecimal
-        return bd.movePointLeft(bd.precision());  // move the decimal point all the way to the left
+        BigDecimal bd = new BigDecimal(bi);
+        return bd.movePointLeft(bd.precision());
     }
 
-    public static BigDecimal nextBigDecimal(BigDecimal norm, int scale, Random r) {
+    private static BigDecimal nextBigDecimal(BigDecimal norm, int scale, Random r) {
         return norm.multiply(nextBigDecimal(scale, r));
     }
 
