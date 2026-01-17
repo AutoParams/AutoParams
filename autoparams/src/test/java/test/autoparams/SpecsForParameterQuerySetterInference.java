b4d308064d294a34b9f58b7370016bc7f8684344
62hoon99 <ske06008@gmail.com>
2026-01-18 00:19:09 +0900

    Split invalid setter test cases into separate tests

--- autoparams/src/test/java/test/autoparams/SpecsForParameterQuerySetterInference.java
+++ autoparams/src/test/java/test/autoparams/SpecsForParameterQuerySetterInference.java
@@ -18,13 +18,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
  */
 public class SpecsForParameterQuerySetterInference {
 
-    @Setter
-    public static class User {
-        private String name;
-        private String userName;
-        private Integer age;
-    }
-
     @Test
     void toLog_infers_property_name_from_setter_method_name_when_parameter_name_is_synthetic() throws Exception {
         Method setter = User.class.getMethod("setName", String.class);
@@ -62,23 +55,90 @@ public class SpecsForParameterQuerySetterInference {
     }
 
     @Test
-    void toLog_uses_parameter_name_when_available_and_not_synthetic() throws Exception {
-        // This test verifies that when parameter name is available and not synthetic,
-        // it uses the parameter name instead of inferring from method name
-        // Note: This test may pass or fail depending on compilation flags (-parameters)
-        Method setter = User.class.getMethod("setName", String.class);
+    void toLog_returns_parameter_name_as_is_when_parameter_name_is_synthetic_and_method_is_not_setter() throws Exception {
+        Method nonSetter = NonSetterTarget.class.getMethod("notSetter", String.class);
+        Parameter parameter = nonSetter.getParameters()[0];
+        ParameterQuery sut = new ParameterQuery(parameter, 0, String.class);
+
+        String actual = sut.toLog(false);
+
+        // Since this is not a setter method, it should keep the synthetic parameter name (e.g., "arg0")
+        assertThat(actual).isEqualTo("String arg0");
+    }
+
+    @Test
+    void toLog_infers_property_name_from_setter_method_name_when_setter_name_is_single_uppercase_character() throws Exception {
+        Method setter = EdgeCaseUser.class.getMethod("setX", String.class);
         Parameter parameter = setter.getParameters()[0];
         ParameterQuery sut = new ParameterQuery(parameter, 0, String.class);
 
         String actual = sut.toLog(false);
 
-        // If parameter name is present and not synthetic, use it
-        // Otherwise, infer from method name
-        if (parameter.isNamePresent() && !parameter.getName().startsWith("arg")) {
-            assertThat(actual).isEqualTo("String " + parameter.getName());
-        } else {
-            // If synthetic, should infer "name" from "setName"
-            assertThat(actual).isEqualTo("String name");
+        // Should infer "x" from "setX"
+        assertThat(actual).isEqualTo("String x");
+    }
+
+    @Test
+    void toLog_infers_property_name_from_setter_method_name_when_setter_name_contains_uppercase_acronym() throws Exception {
+        Method setter = EdgeCaseUser.class.getMethod("setURL", String.class);
+        Parameter parameter = setter.getParameters()[0];
+        ParameterQuery sut = new ParameterQuery(parameter, 0, String.class);
+
+        String actual = sut.toLog(false);
+
+        // Should decapitalize only the first character: "URL" -> "uRL"
+        assertThat(actual).isEqualTo("String uRL");
+    }
+
+    @Test
+    void toLog_does_not_infer_property_name_when_method_name_starts_with_set_but_fourth_character_is_not_uppercase() throws Exception {
+        Method setupMethod = InvalidSetterUser.class.getMethod("setup", String.class);
+        Parameter setupParameter = setupMethod.getParameters()[0];
+        ParameterQuery sut = new ParameterQuery(setupParameter, 0, String.class);
+
+        String actual = sut.toLog(false);
+
+        // "setup" is not a valid setter because the fourth character is not uppercase
+        assertThat(actual).isEqualTo("String arg0");
+    }
+
+    @Test
+    void toLog_does_not_infer_property_name_when_method_has_more_than_one_parameter() throws Exception {
+        Method multiParamMethod = InvalidSetterUser.class.getMethod("setAge", String.class, String.class);
+        Parameter multiParam = multiParamMethod.getParameters()[0];
+        ParameterQuery sut = new ParameterQuery(multiParam, 0, String.class);
+
+        String actual = sut.toLog(false);
+
+        // Method has more than one parameter, so it should not be treated as a setter
+        assertThat(actual).isEqualTo("String arg0");
+    }
+
+    @Setter
+    public static class User {
+        private String name;
+        private String userName;
+        private Integer age;
+    }
+
+    public static class NonSetterTarget {
+        public void notSetter(String value) {
+        }
+    }
+
+    public static class EdgeCaseUser {
+        public void setX(String value) {
+        }
+
+        public void setURL(String value) {
+        }
+    }
+
+    public static class InvalidSetterUser {
+        public void setup(String value) {
+        }
+
+        public void setAge(String a, String b) {
         }
     }
 }
